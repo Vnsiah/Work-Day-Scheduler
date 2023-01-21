b7810f3083f88cf3d4f5bf3130c11ffe2cb6f1ec
Vida Nsiah <vidansiah@Vidas-MacBook-Air.local>
2023-01-21 14:29:20 +0000

    psuedo code

---
+++ images/starter/script.js
@@ -0,0 +1,21 @@
+// We want an array of timeblocks
+// each timeblock will be an object
+// with the shape of 
+// {
+//      hour: 9,
+//      description: "event data"
+// }
+
+// Loop over the array of timeblocks
+// and create html using jQuery
+// for(let i = 0; i < timeblocks.length; i++) {
+    // use jQuery to create a block of html that looks like the following:
+    //
+ 
+//
+//  }
+
+
+
+
+   
